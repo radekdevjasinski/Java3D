5663993cd4896be4b2ef16ab8088e7bf543d5a23
Elizaa0 <elizajanus41@gmail.com>
2023-12-13 13:18:40 +0100

    Zadanie 1 labki 8

---
+++ core/src/com/mygdx/game/PrimitiveObject.java
@@ -0,0 +1,34 @@
+package com.mygdx.game;
+
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.g3d.Material;
+import com.badlogic.gdx.graphics.g3d.Model;
+import com.badlogic.gdx.graphics.g3d.ModelInstance;
+import com.badlogic.gdx.graphics.g3d.attributes.ColorAttribute;
+
+public class PrimitiveObject {
+    private ModelInstance modelInstance;
+
+    public PrimitiveObject(Model model, Color color) {
+        modelInstance = new ModelInstance(model);
+        Material material = modelInstance.materials.get(0);
+        material.set(ColorAttribute.createDiffuse(color));
+    }
+
+    public PrimitiveObject(float[] floats, short[] shorts, java.awt.Color color, ModelInstance modelInstance) {
+        this.modelInstance = modelInstance;
+    }
+
+    public PrimitiveObject(java.awt.Color color) {
+    }
+
+    public void dispose() {
+        // Dispose resources if needed
+    }
+
+    public ModelInstance getModelInstance() {
+        return modelInstance;
+    }
+}
+
+
---
+++ core/src/com/mygdx/game/Sphere.java
@@ -0,0 +1,11 @@
+package com.mygdx.game;
+
+import java.awt.*;
+
+public class Sphere extends PrimitiveObject {
+    public Sphere(float radius, int segments, Color color) {
+        // Implementacja generowania wierzchołków i indeksów dla sfery
+        // ... (możesz użyć funkcji matematycznych do generowania współrzędnych sfery)
+        super(color);
+    }
+}
