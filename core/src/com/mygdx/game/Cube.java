c3c7d7979c2607a4fe36dec1a664319dbff85de1
Elizaa0 <elizajanus41@gmail.com>
2023-12-13 14:19:37 +0100

    Zadanie 2 labki 8

--- core/src/com/mygdx/game/Cube.java
+++ core/src/com/mygdx/game/Cube.java
@@ -1,21 +1,52 @@
 package com.mygdx.game;
 
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Mesh;
+import com.badlogic.gdx.graphics.VertexAttributes;
+import com.badlogic.gdx.graphics.g3d.Material;
+import com.badlogic.gdx.graphics.g3d.Model;
 import com.badlogic.gdx.graphics.g3d.ModelInstance;
+import com.badlogic.gdx.graphics.g3d.utils.MeshBuilder;
+import com.badlogic.gdx.graphics.g3d.utils.ModelBuilder;
 
-import java.awt.*;
 
-public class Cube extends PrimitiveObject {
-    private static ModelInstance modelInstance;
+public class Cube extends ModelInstance {
 
     public Cube(float size, Color color) {
-        super(new float[]{
-                -size / 2, -size / 2, -size / 2,
-                size / 2, -size / 2, -size / 2,
-                size / 2, size / 2, -size / 2,
-                // ... (dodaj resztę wierzchołków sześcianu)
-        }, new short[]{
-                0, 1, 2,
-                // ... (dodaj resztę indeksów sześcianu)
-        }, color, modelInstance);
+        super(createCubeModel(size, color));
+    }
+
+    private static Model createCubeModel(float size, Color color) {
+        ModelBuilder modelBuilder = new ModelBuilder();
+        modelBuilder.begin();
+
+        int attr = VertexAttributes.Usage.Position | VertexAttributes.Usage.Normal | VertexAttributes.Usage.ColorPacked;
+
+        // Define vertices
+        float[] vertices = {
+                -size / 2f, -size / 2f, -size / 2f, 0, 0, -1, Color.toFloatBits(color.r, color.g, color.b, color.a),
+                size / 2f, -size / 2f, -size / 2f, 0, 0, -1, Color.toFloatBits(color.r, color.g, color.b, color.a),
+                size / 2f, size / 2f, -size / 2f, 0, 0, -1, Color.toFloatBits(color.r, color.g, color.b, color.a),
+                -size / 2f, size / 2f, -size / 2f, 0, 0, -1, Color.toFloatBits(color.r, color.g, color.b, color.a),
+
+        };
+
+        // Define indices
+        short[] indices = {
+                0, 1, 2, 2, 3, 0,
+        };
+
+        MeshBuilder meshBuilder = new MeshBuilder();
+        meshBuilder.begin(attr, GL20.GL_TRIANGLES);
+        meshBuilder.part("cube", GL20.GL_TRIANGLES);
+
+        Mesh mesh = meshBuilder.end();
+
+        modelBuilder.part("cube", mesh, GL20.GL_TRIANGLES, new Material());
+
+        modelBuilder.end();
+
+        return modelBuilder.end();
     }
 }
