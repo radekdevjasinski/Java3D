dd6fd5fe611da669369dd3abbc58c5f7f8908394
radek.jasiu.jasinski@gmail.com <radek.jasiu.jasinski@gmail.com>
2024-01-17 07:34:58 +0100

    models

--- core/src/com/mygdx/game/Engine.java
+++ core/src/com/mygdx/game/Engine.java
@@ -10,10 +10,14 @@ import com.badlogic.gdx.graphics.VertexAttributes.Usage;
 import com.badlogic.gdx.graphics.g3d.*;
 import com.badlogic.gdx.graphics.g3d.attributes.ColorAttribute;
 import com.badlogic.gdx.graphics.g3d.environment.DirectionalLight;
+import com.badlogic.gdx.graphics.g3d.utils.MeshPartBuilder;
 import com.badlogic.gdx.graphics.g3d.utils.ModelBuilder;
 import com.badlogic.gdx.math.Quaternion;
 import com.badlogic.gdx.math.Vector3;
 import com.badlogic.gdx.utils.Array;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class Engine extends ApplicationAdapter {
 	private PerspectiveCamera camera;
@@ -24,7 +28,7 @@ public class Engine extends ApplicationAdapter {
 
 	@Override
 	public void create() {
-		camera = new PerspectiveCamera(67, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+		camera = new PerspectiveCamera(100, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
 		camera.position.set(10f, 10f, 10f);
 		camera.lookAt(0, 0, 0);
 		camera.near = 1f;
@@ -39,9 +43,11 @@ public class Engine extends ApplicationAdapter {
 		objects = new Array<>();
 		objects.add(new PrimitiveObject(createCubeModel(), Color.RED));
 		objects.add(new PrimitiveObject(createSphereModel(), Color.GREEN));
+		objects.add(new PrimitiveObject(createIrregularShapeModel(Color.GOLD), Color.GOLD));
 
-		objects.get(0).getModelInstance().transform.set(new Vector3(-3,0,3), new Quaternion());
-		objects.get(1).getModelInstance().transform.set(new Vector3(3,0,-3), new Quaternion());
+		objects.get(0).getModelInstance().transform.set(new Vector3(-7,0,7), new Quaternion());
+		objects.get(1).getModelInstance().transform.set(new Vector3(0,0,0), new Quaternion());
+		objects.get(2).getModelInstance().transform.set(new Vector3(7,0,-7), new Quaternion());
 
 	}
 
@@ -54,6 +60,37 @@ public class Engine extends ApplicationAdapter {
 		ModelBuilder modelBuilder = new ModelBuilder();
 		return modelBuilder.createSphere(7f, 7f, 7f, 20, 20, new Material(ColorAttribute.createDiffuse(Color.GREEN)), Usage.Position | Usage.Normal);
 	}
+
+	private Model createIrregularShapeModel(Color color) {
+		ModelBuilder modelBuilder = new ModelBuilder();
+		List<Model> models = new ArrayList<>();
+/*
+		// Tworzenie sześcianu
+		modelBuilder.begin();
+		MeshPartBuilder partBuilder = modelBuilder.part("box", GL20.GL_TRIANGLES, Usage.Position | Usage.Normal, new Material(ColorAttribute.createDiffuse(color)));
+		partBuilder.box(4f, 4f, 4f);
+		models.add(modelBuilder.end());
+*/
+		// Tworzenie stożka
+		modelBuilder.begin();
+		MeshPartBuilder partBuilder = modelBuilder.part("cone", GL20.GL_TRIANGLES, Usage.Position | Usage.Normal, new Material(ColorAttribute.createDiffuse(color)));
+		partBuilder.cone(5f, 10f, 5f, 10);
+		models.add(modelBuilder.end());
+
+		models.add(createCubeModel());
+
+		// Łączenie modeli
+		Model finalModel = new Model();
+		for(Model model : models)
+		{
+			finalModel.nodes.addAll(model.nodes);
+			finalModel.meshes.addAll(model.meshes);
+			finalModel.materials.addAll(model.materials);
+
+		}
+		return finalModel;
+	}
+
 
 	@Override
 	public void render() {
